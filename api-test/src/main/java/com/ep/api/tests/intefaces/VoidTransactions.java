b12d5b594c87b71373c5c52cfd0ceeec22a41eca
tttsonev <tsvetalint@gmail.com>
2020-04-02 10:26:47 +0300

    Delete VoidTransactions.java
    common Transaction interface created

--- api-test/src/main/java/com/ep/api/tests/intefaces/VoidTransactions.java
+++
@@ -1,9 +0,0 @@
-package com.ep.api.tests.intefaces;
-
-import org.json.JSONObject;
-
-import java.util.Map;
-
-public interface VoidTransactions {
-    JSONObject makeVoidTransaction(Map<String, String> properties);
-}
